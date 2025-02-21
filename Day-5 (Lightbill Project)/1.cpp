bc368725381e5d150330b8aba85f87c8c257d351
AnkitSabariya <ankitsabariya@gmail.com>
2025-02-21 12:25:49 +0530

    today

--- Day-5 (Lightbill Project)/1.cpp
+++ Day-5 (Lightbill Project)/1.cpp
@@ -2,52 +2,51 @@
 using namespace std;
 int main()
 {
-    int user, temp = 0, sum = 0, n = 0;
-    float arr[] = {50 * 0.50, 100 * 0.75, 150 * 1.20};
-    cout << "Total Electricity Unit : ";
-    cin >> user;
+    //     Write a C++ program to input electricity unit charges and calculate total electricity bill according to the given condition:
+    // • For first 50 units Rs. 0.50/unit
 
-    if (user >= 50)
+    // • For next 100 units Rs. 0.75/unit
+
+    // • For next 100 units Rs. 1.20/unit
+
+    // . For unit above 250 Rs. 1.50/unit
+
+    // • An additional surcharge of 20% is added to the bill
+    int unit;
+    float bill  = 0;
+    cout << "Enter Total bill Of unit : ";
+    cin >> unit;
+    // • For first 50 units Rs. 0.50/unit
+    if (0 < unit && unit <= 50)
     {
-        user = user - 50; // 550
-        temp = user;      // 500
-        cout << "Total unit : " << temp << endl;
-
-        cout << arr[0] << endl;
-
-        if (user >= 150) // 500
-        {
-            user = user - 50;
-            temp = user;
-            cout << "Total unit : " << temp << endl;
-
-            cout << arr[1] << endl;
-
-            if (user >= 250)
-            {
-                user = user - 150;
-                temp = user;
-                cout << "Total unit : " << temp << endl;
-                cout << arr[2] << endl;
-                if (user >= 150)
-                {
-                    user = user;
-                    temp = user;
-                    cout << "Total unit : " << temp << endl;
-                }
-            }
-        }
+        bill = unit * 0.5;
+        cout<<"First under 50 unit Of Charge : " << bill << endl; 
     }
+    // • For next 100 units Rs. 0.75/unit
+    else if (unit > 50 && unit <=150)
+    {
 
-    int per,m,o,f;
-    per = user - 300;
-    n = user * 1.50;
-    cout << n << endl;
-    m = arr[0] + arr[1] + arr[2] + n;
-    cout << "total Rupees Per Charge : " << m << endl;
-    o = (m*20)/100;
-    cout << "Average : " << o << endl;
-    f = m+o;
+        bill = (50 * 0.5) + ((unit - 50) * 0.75);
+        cout<<"51 To 150 Next 100 unit Of Charge : " << bill << endl; 
+    }
+    // • For next 150 units Rs. 1.20/unit
+    else if (unit > 150 && unit <= 250)
+    {
 
-    cout << "total Amount OF Bill : " << f << endl;
+        bill = (50 * 0.5) + (100 * 0.75) + ((unit - 150) * 1.20);
+        cout<<"151 To 300 Next 100 unit Of Charge : " << bill << endl; 
+    }
+    // . For unit above 250 Rs. 1.50/unit
+    else
+    {
+        bill = (50 * 0.5) + (100 * 0.75) + (100 * 1.20) + ((unit - 250) * 1.50);
+        cout<<"For unit above 250 Unit : " << bill << endl; 
+    }
+    cout<<"---------------------------------------------------------------------------------"<<endl;
+     // Add a surcharge of 20%
+     float totalBill = bill + (bill * 0.20);
+     cout << "------------------------------------------------------" << endl;
+     cout << "Your Total Electricity Bill: Rs. " << totalBill << endl;
+ 
+    
 }
